6a5a761572662ee119da9f16cbd28542e1af837f
CortezDeveloper <josecortezsoftware@gmail.com>
2023-08-17 00:13:51 +0200

    Completed Lab

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,39 @@
 // Your code here ...
+const { Schema, model } = require("mongoose")
+
+const recipeSchema = new Schema({
+    title:{
+        type: String,
+        require: true,
+    },
+    instructions:{
+        type: String, 
+        require: true, 
+    },
+    level:{
+        type: String,
+        enum: ["Ease Peasy", "Amateur Chef", "Ultra Pro Chef"],
+    },
+    ingredients:{
+        type: [String],      
+    },
+    image:{
+        type: String,
+        default: "https://images.media-allrecipes.com/images/75131.jpg",
+    },
+    duration: {
+        type: Number,
+        min: 0,
+    },
+    isArchived:{
+        type: Boolean,
+        default: false,
+    },
+    created:{
+        type: Date, 
+        default: Date.now,
+    } 
+})
+
+const Recipe = model("Recipe", recipeSchema)
+module.exports = Recipe
--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,27 +1,5 @@
-const express = require("express");
-const logger = require("morgan");
-const app = express();
-// MIDDLEWARE
-app.use(logger("dev"));
-app.use(express.static("public"));
-app.use(express.json());
 
-const Recipe = require("./models/Recipe.model")
-// Iteration 1 - Connect to MongoDB
-// DATABASE CONNECTION
-const mongoose = require("mongoose");
-const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
-
-mongoose 
-    .connect(MONGODB_URI)
-    .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-    .catch((err) => console.error("Error connecting to mongo", err));
-// ROUTES
-//  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
-});
-
+const router = require("express").Router()
 
 app.post("/recipes",(req, res) => {
     Recipe.create({
@@ -82,43 +60,3 @@ app.delete("/recipes/:id", (req, res) => {
             res.status(500).json({message: "Error while deleting a single recipe"})
         })
 })
-//  Iteration 3 - Create a Recipe route
-//  POST  /recipes route
-
-
-//  Iteration 4 - Get All Recipes
-//  GET  /recipes route
-
-
-//  Iteration 5 - Get a Single Recipe
-//  GET  /recipes/:id route
-
-
-//  Iteration 6 - Update a Single Recipe
-//  PUT  /recipes/:id route
-
-
-//  Iteration 7 - Delete a Single Recipe
-//  DELETE  /recipes/:id route
-
-
-// BONUS
-//  Bonus: Iteration 9 - Create a Single User
-//  POST  /users route
-
-
-//  Bonus: Iteration 10 | Get a Single User
-//  GET /users/:id route
-
-
-//  Bonus: Iteration 11 | Update a Single User
-//  GET /users/:id route
-
-
-// Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
-
-
-
-//❗️DO NOT REMOVE THE BELOW CODE
-module.exports = app;
